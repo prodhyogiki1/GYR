88e32651587b6805d61a5af8e2d29b482a5b7ea9
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-02-17 11:51:05 +0530

    comma in verify agent profile query

--- class/Agent.php
+++ class/Agent.php
@@ -93,7 +93,7 @@ function verify_agent_profile($fname,$lname,$phone,$email,$designation,$phone2,$
     }
     
 
-    echo $query = "update agent set fname='$fname',lname='$lname',phone='$phone',email='$email',designation='$designation',phone2='$phone2',bname='$bname',baddress='$baddress',landmark='$landmark',country='$country',state='$state',city='$city',google_business_link='$google_business_link',gstin='$gstin',pan='$pan',business_licence='$business_licence',pan_file='$pan_file',gstin_file='$gstin_file',business_licence_file='$business_licence_file', where id='$id' ";
+    $query = "update agent set fname='$fname',lname='$lname',phone='$phone',email='$email',designation='$designation',phone2='$phone2',bname='$bname',baddress='$baddress',landmark='$landmark',country='$country',state='$state',city='$city',google_business_link='$google_business_link',gstin='$gstin',pan='$pan',business_licence='$business_licence',pan_file='$pan_file',gstin_file='$gstin_file',business_licence_file='$business_licence_file' where id='$id' ";
     $result=$this->db_handle->update($query);
     return $result;
 }
